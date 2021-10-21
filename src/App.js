d40aca7e8119d3f61b15ff5da8d58152694efa55
Jethro Williams <jethrowilliams@outlook.com>
2021-10-21 11:31:33 +0100

    Use useCallback() with React.memo to not re-evaluate a component with a function as a prop, each time the parent component is re-evaluated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 
 import Button from './components/UI/Button/Button';
 import DemoOutput from './components/Demo/DemoOutput';
@@ -9,9 +9,27 @@ function App() {
 
   console.log('APP RUNNING');
 
-  const toggleParagraphHandler = () => {
+  const toggleParagraphHandler = useCallback(() => {
     setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-  };
+  }, []);
+  // useCallback allows us to store a function across component executions (so one function is saved, and the function is not recreated (as a new function) with every execution)
+
+  // For example, without useCallback, we would do the following with each re-evaluation:
+  // let initialObject = {name: 'Jethro'}
+  // let currentObject = {name: 'Jethro'}
+  // 'initialObject === currentObject' returns false (as it is not pointing at the same object in memory)
+
+  // With useCallback, we do:
+  // let initialObject = {name: 'Jethro'}
+  // let currentObject = initialObject
+  // 'initialObject === currentObject' returns true
+
+  // Therefore, if we know that a function will never change, we can use useCallback to store it
+
+  // The second argument is an array of dependencies (an empty array here) used in the same way as with useEffect
+  // These determine when toggleParagraphHandler should re-run
+  // If it is an empty array, it will never be re-run, meaning that the function stored in toggleParagraphHandler will never change
+
 
   return (
     <div className="app">
