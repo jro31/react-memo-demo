b5c53a3f3d881d6612fd16d62f7f4f7e6f6e5857
Jethro Williams <jethrowilliams@outlook.com>
2021-10-21 12:08:09 +0100

    Use useCallback with dependency

--- src/App.js
+++ src/App.js
@@ -6,35 +6,29 @@ import './App.css';
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
   console.log('APP RUNNING');
 
   const toggleParagraphHandler = useCallback(() => {
-    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-  }, []);
-  // useCallback allows us to store a function across component executions (so one function is saved, and the function is not recreated (as a new function) with every execution)
+    if (allowToggle) {
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    }
+  }, [allowToggle]);
+  // When this function is initially stored in toggleParagraphHandler, allowToggle is 'false'
+  // Without any dependencies, even if 'allowToggle' was updated (to true, for example), it would still be stored as false within this function
+  // By adding 'allowToggle' as a dependency, it tells React to recreate this function any time that allowToggle changes
 
-  // For example, without useCallback, we would do the following with each re-evaluation:
-  // let initialObject = {name: 'Jethro'}
-  // let currentObject = {name: 'Jethro'}
-  // 'initialObject === currentObject' returns false (as it is not pointing at the same object in memory)
 
-  // With useCallback, we do:
-  // let initialObject = {name: 'Jethro'}
-  // let currentObject = initialObject
-  // 'initialObject === currentObject' returns true
-
-  // Therefore, if we know that a function will never change, we can use useCallback to store it
-
-  // The second argument is an array of dependencies (an empty array here) used in the same way as with useEffect
-  // These determine when toggleParagraphHandler should re-run
-  // If it is an empty array, it will never be re-run, meaning that the function stored in toggleParagraphHandler will never change
-
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  };
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={false} />
+      <DemoOutput show={showParagraph} />
+      <Button onClick={allowToggleHandler}>Allow toggling</Button>
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
